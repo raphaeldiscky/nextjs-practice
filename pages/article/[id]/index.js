72b9b9c0f22fa2167b35eb23fbf1e733e603163e
Raphael Discky <zundria.putra@gmail.com>
2021-02-19 21:58:26 +0700

    Combine getStaticProps and getStaticPaths

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -15,7 +15,7 @@ const article = ({ article }) => {
   )
 }
 
-export const getServerSideProps = async (context) => {
+export const getStaticProps = async (context) => {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
   )
@@ -24,6 +24,20 @@ export const getServerSideProps = async (context) => {
     props: {
       article
     }
+  }
+}
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+  const articles = await res.json()
+  const ids = articles.map((article) => article.id)
+  console.log(ids)
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }))
+  console.log(paths)
+
+  return {
+    paths,
+    fallback: false
   }
 }
 
