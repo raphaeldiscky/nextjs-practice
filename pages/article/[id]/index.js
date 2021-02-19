02cead6115211f95bae58d24bf244b602e2407df
Raphael Discky <zundria.putra@gmail.com>
2021-02-19 22:49:31 +0700

    Create routes and use api data

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,5 +1,6 @@
 // import { useRouter } from 'next/router'
 import Link from 'next/link'
+import { server } from '../../../config'
 
 const article = ({ article }) => {
   //   const router = useRouter()
@@ -16,9 +17,7 @@ const article = ({ article }) => {
 }
 
 export const getStaticProps = async (context) => {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
-  )
+  const res = await fetch(`${server}/api/articles/${context.params.id}`)
   const article = await res.json()
   return {
     props: {
@@ -28,7 +27,7 @@ export const getStaticProps = async (context) => {
 }
 
 export const getStaticPaths = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+  const res = await fetch(`${server}/api/articles`)
   const articles = await res.json()
   const ids = articles.map((article) => article.id)
   const paths = ids.map((id) => ({ params: { id: id.toString() } }))
@@ -38,5 +37,29 @@ export const getStaticPaths = async () => {
     fallback: false
   }
 }
+
+// export const getStaticProps = async (context) => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+//   )
+//   const article = await res.json()
+//   return {
+//     props: {
+//       article
+//     }
+//   }
+// }
+
+// export const getStaticPaths = async () => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+//   const articles = await res.json()
+//   const ids = articles.map((article) => article.id)
+//   const paths = ids.map((id) => ({ params: { id: id.toString() } }))
+
+//   return {
+//     paths,
+//     fallback: false
+//   }
+// }
 
 export default article
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import ArticleList from '../components/ArticleList'
+import { server } from '../config'
 
 export default function Home({ articles }) {
   // console.log(articles)
@@ -15,7 +16,7 @@ export default function Home({ articles }) {
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const res = await fetch(`${server}/api/articles`)
   const articles = await res.json()
 
   return {
@@ -24,3 +25,14 @@ export const getStaticProps = async () => {
     }
   }
 }
+
+// export const getStaticProps = async () => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+//   const articles = await res.json()
+
+//   return {
+//     props: {
+//       articles
+//     }
+//   }
+// }
