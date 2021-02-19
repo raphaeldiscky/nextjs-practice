d59d413d1e5f0c128d0846e84711ef54f7f0e413
Raphael Discky <zundria.putra@gmail.com>
2021-02-19 22:34:57 +0700

    Remove 'as' in link

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const ArticleItem = ({ article }) => {
   return (
-    <Link href='/article/[id]' as={`/article/${article.id}`}>
+    <Link href={`/article/${article.id}`}>
       <a className={articleStyles.card}>
         <h3>{article.title} &rarr;</h3>
         <p>{article.body}</p>
